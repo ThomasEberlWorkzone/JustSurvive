949fa8a167a36e63fbf6d711fd5a23477b1789f7
Thomas Eberl <ebethd14@htlkaindorf.at>
2017-10-12 12:43:22 +0200

    Added management for items

---
+++ src/main/java/com/pts/justsurvive/management/items/ManageItems.java
@@ -0,0 +1,9 @@
+package com.pts.justsurvive.management.items;
+
+import com.pts.justsurvive.items.Bandage;
+import net.minecraft.item.Item;
+
+public class ManageItems
+{
+    public static final Item ITEM_BANDAGE = new Bandage("bandage");
+}
