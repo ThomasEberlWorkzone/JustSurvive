543e2fac256b3f26f4cb3422847e3e327d92940e
Thomas Eberl <ebethd14@htlkaindorf.at>
2017-10-12 15:25:17 +0200

    Addes Recipe for bandage

--- src/main/java/com/pts/justsurvive/proxy/ClientProxy.java
+++ src/main/java/com/pts/justsurvive/proxy/ClientProxy.java
@@ -1,10 +1,32 @@
 package com.pts.justsurvive.proxy;
 
+import com.pts.justsurvive.management.items.ManageItems;
+import net.minecraft.init.Blocks;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemStack;
+import net.minecraft.util.ResourceLocation;
 import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
+import net.minecraftforge.fml.common.registry.GameRegistry;
 import net.minecraftforge.fml.relauncher.Side;
 
 @Mod.EventBusSubscriber(Side.CLIENT)
 public class ClientProxy extends CommonProxy
 {
+    @Override
+    public void preInit(FMLPreInitializationEvent event)
+    {
+        super.preInit(event);
 
+        addRecipeForBandage(ManageItems.ITEM_BANDAGE);
+    }
+
+    private void addRecipeForBandage(Item itemToBeCrafted)
+    {
+        GameRegistry.addShapedRecipe(new ResourceLocation("JustSurvive"), null, new ItemStack(itemToBeCrafted),
+                                     "X X",
+                                             " X ",
+                                             "X X",
+                                             'X', Blocks.WOOL);
+    }
 }
