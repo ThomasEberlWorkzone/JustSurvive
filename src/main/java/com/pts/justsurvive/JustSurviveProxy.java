16e0ac95f6cf8b9fa7e687f986ec27e2a553b1b3
Thomas Eberl <ebethd14@htlkaindorf.at>
2017-10-12 12:01:15 +0200

    Added Proxy Structure

--- src/main/java/com/pts/justsurvive/JustSurviveProxy.java
+++ src/main/java/com/pts/justsurvive/JustSurviveProxy.java
@@ -3,6 +3,9 @@ package com.pts.justsurvive;
 import com.pts.justsurvive.proxy.CommonProxy;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.common.SidedProxy;
+import net.minecraftforge.fml.common.event.FMLInitializationEvent;
+import net.minecraftforge.fml.common.event.FMLPostInitializationEvent;
+import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
 
 @Mod(modid = JustSurviveProxy.MODID, version = JustSurviveProxy.VERSION)
 public class JustSurviveProxy
@@ -15,4 +18,22 @@ public class JustSurviveProxy
 
     @SidedProxy(clientSide = "com.pts.justsurvive.proxy.ClientProxy", serverSide = "com.pts.justsurvive.proxy.ServerProxy")
     public static CommonProxy proxy;
+
+    @Mod.EventHandler
+    public void preInit(FMLPreInitializationEvent event)
+    {
+        proxy.preInit(event);
+    }
+
+    @Mod.EventHandler
+    public void init(FMLInitializationEvent event)
+    {
+        proxy.init(event);
+    }
+
+    @Mod.EventHandler
+    public void postInit(FMLPostInitializationEvent event)
+    {
+        proxy.postInit(event);
+    }
 }
