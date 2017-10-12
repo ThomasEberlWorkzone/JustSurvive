7539ba44904580f91240afc664adf3d4538d0ce1
Thomas Eberl <ebethd14@htlkaindorf.at>
2017-10-12 12:01:39 +0200

    Remaining Proxy Structure

--- src/main/java/com/pts/justsurvive/proxy/CommonProxy.java
+++ src/main/java/com/pts/justsurvive/proxy/CommonProxy.java
@@ -1,6 +1,43 @@
 package com.pts.justsurvive.proxy;
 
+import com.pts.justsurvive.JustSurviveProxy;
+import net.minecraft.block.Block;
+import net.minecraft.item.Item;
+import net.minecraftforge.client.event.ModelRegistryEvent;
+import net.minecraftforge.event.RegistryEvent;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.common.event.FMLInitializationEvent;
+import net.minecraftforge.fml.common.event.FMLPostInitializationEvent;
+import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
+import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
+
+@Mod.EventBusSubscriber(modid = JustSurviveProxy.MODID)
 public class CommonProxy
 {
+    public void preInit(FMLPreInitializationEvent event) {}
+    public void init(FMLInitializationEvent event) {}
+    public void postInit(FMLPostInitializationEvent event) {}
 
+    @SubscribeEvent
+    public static void registerBlocks(RegistryEvent.Register<Block> event)
+    {
+
+    }
+
+    @SubscribeEvent
+    public static void registerItems(RegistryEvent.Register<Item> event)
+    {
+
+    }
+
+    @SubscribeEvent
+    public static void registerRenders(ModelRegistryEvent event)
+    {
+
+    }
+
+    public static void registerRender(Item item)
+    {
+
+    }
 }
--- src/main/java/com/pts/justsurvive/proxy/ServerProxy.java
+++ src/main/java/com/pts/justsurvive/proxy/ServerProxy.java
@@ -1,5 +1,9 @@
 package com.pts.justsurvive.proxy;
 
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.relauncher.Side;
+
+@Mod.EventBusSubscriber(Side.SERVER)
 public class ServerProxy
 {
 
