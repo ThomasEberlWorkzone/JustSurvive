d503a90d1d468fdaecaf551d8bc870284d9063e5
Thomas Eberl <ebethd14@htlkaindorf.at>
2017-10-13 10:55:29 +0200

    Updated using bandages by using rightclick instead of right and leftclick

--- src/main/java/com/pts/justsurvive/eventhandler/ItemEventHandler.java
+++ src/main/java/com/pts/justsurvive/eventhandler/ItemEventHandler.java
@@ -2,6 +2,7 @@ package com.pts.justsurvive.eventhandler;
 
 import com.pts.justsurvive.management.items.ManageItems;
 import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.item.ItemStack;
 import net.minecraftforge.event.entity.player.PlayerInteractEvent;
 import net.minecraftforge.fml.common.eventhandler.EventPriority;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
@@ -14,7 +15,7 @@ public class ItemEventHandler
 {
     //This Method is called when a Player Rightclicks with an Item in Hand
     @SubscribeEvent(priority = EventPriority.HIGH)
-    public void onBandageIsUsed(PlayerInteractEvent event)
+    public void onBandageIsUsed(PlayerInteractEvent.RightClickItem event)
     {
         EntityPlayer p = event.getEntityPlayer();
 
