f15fabb69358860cc79b0463f19bd1785f8ed44c
ThomasEberl <thomas.eberl@sendhybrid.com>
2017-10-13 16:04:09 +0200

    Tried fixing biome change event

--- src/main/java/com/pts/justsurvive/eventhandler/MechanicEventHandler.java
+++ src/main/java/com/pts/justsurvive/eventhandler/MechanicEventHandler.java
@@ -4,12 +4,14 @@ package com.pts.justsurvive.eventhandler;
     This class is used to handle events of all different mechanics
  */
 
+import net.minecraftforge.event.entity.living.LivingEvent;
+import net.minecraftforge.fml.common.eventhandler.EventPriority;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
 
 public class MechanicEventHandler
 {
-    @SubscribeEvent
-    public void onEntryToColdBiome()
+    @SubscribeEvent(priority = EventPriority.HIGH)
+    public void onEntryToColdBiome(LivingEvent.LivingUpdateEvent event)
     {
 
     }
