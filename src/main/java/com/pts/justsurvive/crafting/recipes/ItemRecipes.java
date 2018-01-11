587ff7c81929242a1610c35a08a12a1b6b2ac9b6
Thomas Eberl <ebethd14@htlkaindorf.at>
2018-01-11 07:52:34 +0100

    addes recipes

--- src/main/java/com/pts/justsurvive/crafting/recipes/ItemRecipes.java
+++ src/main/java/com/pts/justsurvive/crafting/recipes/ItemRecipes.java
@@ -1,6 +1,8 @@
 package com.pts.justsurvive.crafting.recipes;
 
+import com.pts.justsurvive.management.items.ManageItems;
 import net.minecraft.init.Blocks;
+import net.minecraft.init.Items;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
 import net.minecraft.util.ResourceLocation;
@@ -11,24 +13,38 @@ public class ItemRecipes
     public void addRecipeForBandage(Item itemToBeCrafted)
     {
         GameRegistry.addShapedRecipe(new ResourceLocation("JustSurvive"), null, new ItemStack(itemToBeCrafted),
-                "X X",
+                " X ",
+                        "XXX",
                         " X ",
-                        "X X",
                         'X', Blocks.WOOL);
     }
 
     public void addRecipeForAdrenalin(Item itemToBeCrafted)
     {
-
+        GameRegistry.addShapedRecipe(new ResourceLocation("JustSurvive"), null, new ItemStack(itemToBeCrafted),
+                "  Z",
+                        "XYZ",
+                        "  z",
+                        'X', Blocks.GLASS,
+                        'Y', new ItemStack(Items.DYE,1,2),
+                        'Z', Items.IRON_INGOT);
     }
 
     public void addRecipeForAntibiotics(Item itemToBeCrafted)
     {
-
+        GameRegistry.addShapedRecipe(new ResourceLocation("JustSurvive"), null, new ItemStack(itemToBeCrafted),
+                "   ",
+                        "   ",
+                        "XXX",
+                        'X', ManageItems.ITEM_BANDAGE);
     }
 
     public void addRecipeForSplint(Item itemToBeCrafted)
     {
-
+        GameRegistry.addShapedRecipe(new ResourceLocation("JustSurvive"), null, new ItemStack(itemToBeCrafted),
+                "XXX",
+                        "X  ",
+                        "XXX",
+                        'X', Items.STICK);
     }
 }
